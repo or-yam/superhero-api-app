e0680c60bff1c8807c52bd9f6504f272aff67d25
Oryam <48219965+or-yam@users.noreply.github.com>
2020-04-07 10:44:21 +0300

    added some styling and changed font

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { characters } from './characters';
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -24,7 +25,7 @@ class App extends Component {
     });
     return (
       <div className="tc">
-        <h1>Star Wars Characters</h1>
+        <h1 className='f1'>Star Wars Characters</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList characters={filterCharacters} />
       </div>
