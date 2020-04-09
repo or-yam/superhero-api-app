229cee11ae7b02fb9a5e19e48db7edc1a9301567
Oryam <48219965+or-yam@users.noreply.github.com>
2020-04-09 22:29:15 +0300

    some distracturing

---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      characters: [],
+      searchField: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://akabab.github.io/superhero-api/api/all.json')
+      .then((response) => response.json())
+      .then((characters) => this.setState({ characters: characters }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const { characters, searchField } = this.state;
+    const filterCharacters = characters.filter((character) => {
+      return character.name
+        .toLocaleLowerCase()
+        .includes(searchField.toLocaleLowerCase());
+    });
+    return !characters.length ? (
+      <div className="tc">
+        <h1>Loading...</h1>
+      </div>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">Star Wars Characters</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList characters={filterCharacters} />
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
