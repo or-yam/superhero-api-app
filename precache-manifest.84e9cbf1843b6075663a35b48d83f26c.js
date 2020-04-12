0d2768b59e9e844ab932cab387f36f446f939444
Oryam <48219965+or-yam@users.noreply.github.com>
2020-04-12 13:02:04 +0300

    Updates

---
+++ precache-manifest.84e9cbf1843b6075663a35b48d83f26c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1090547dc04ddbced50e02bc9c71d716",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b224d06af255f940fd74",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "98f72c93da4a996c0d8a",
+    "url": "/static/css/main.4110d6ad.chunk.css"
+  },
+  {
+    "revision": "b224d06af255f940fd74",
+    "url": "/static/js/2.c4c66daf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4c66daf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98f72c93da4a996c0d8a",
+    "url": "/static/js/main.6a5f546a.chunk.js"
+  },
+  {
+    "revision": "507a706e988269f713cc",
+    "url": "/static/js/runtime-main.2604d2a5.js"
+  },
+  {
+    "revision": "c6a0f798b2e69e6ab0ae025fddef4152",
+    "url": "/static/media/intsh.c6a0f798.woff"
+  }
+]);
